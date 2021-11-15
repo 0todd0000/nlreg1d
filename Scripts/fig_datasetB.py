69e7b91d3c86b0f9af4e52a94a6ab678d0de9721
Todd Pataky <todd@todds-imac-pro.lan>
2021-11-15 10:56:43 +0900

    Update figure scripts

--- Scripts/fig_datasetB.py
+++ Scripts/fig_datasetB.py
@@ -12,30 +12,6 @@ save      = True
 
 
 
-# Dataset A (simulated)
-fnameCSV  = os.path.join( dirDATA, 'SimulatedA.csv')
-a         = np.loadtxt( fnameCSV, delimiter=',')
-g,y       = a[:,0], a[:,1:]  # group, dependent variable
-J         = (g==0).sum()     # number of observations in first group
-### register:
-Q         = y.shape[1]
-q         = np.linspace(0, 1, Q)
-yr,wr     = nlreg1d.reg.fpca( y, ncomp=5, smooth=False, niter=niter )
-### plot:
-plt.close('all')
-colors    = '0.0', (0.3,0.5,0.99)
-ylimt     = (-7.5, 7.5)
-ylim      = [ (-5, 35), (-5, 35), (-19, 59), ylimt, (-2, 50), ylimt, ylimt ]
-alpha_loc = [  [(70,4.5), (70,3.3)]  , [(70,32), (70,28)]  ,  [(70,4.6), (70,3.4)]   ,    [(70,5.1), (70,4.0)]  ]
-nlreg1d.plot.plot_multipanel(y, yr, wr, J, colors, ylim, alpha_loc, paired=False, dvlabel='Dependent variable value')
-plt.show()
-if save:
-	dirFIGS  = os.path.join( pathlib.Path( __file__ ).parent.parent, 'Figures')
-	plt.savefig( os.path.join(dirFIGS, 'datasetA.pdf')  )
-
-
-
-
 # Dataset B (simulated)
 fnameCSV  = os.path.join( dirDATA, 'SimulatedB.csv')
 a         = np.loadtxt( fnameCSV, delimiter=',')
---
+++ Scripts/fig_datasetD.py
@@ -0,0 +1,39 @@
+
+import os,pathlib
+import numpy as np
+from matplotlib import pyplot as plt
+import spm1d
+import nlreg1d
+
+
+dirDATA   = os.path.join( pathlib.Path( __file__ ).parent.parent, 'Data')
+niter     = 5
+save      = True
+
+
+
+# Dataset D:  COP (Pataky et al., 2014)
+fnameCSV  = os.path.join( dirDATA, 'Pataky2014-mediolateral.csv')
+a         = np.loadtxt( fnameCSV, delimiter=',')
+g,y       = a[:,0], a[:,1:]  # group, dependent variable
+J         = (g==0).sum()     # number of observations in first group
+### register:
+Q         = y.shape[1]
+q         = np.linspace(0, 1, Q)
+yr,wr     = nlreg1d.reg.fpca( y, ncomp=5, smooth=False, niter=niter )
+### plot:
+plt.close('all')
+colors    = '0.0', (0.8,0.1,0.1)
+xlabel    = 'Time  (% stance)'
+ylimt     = (-7, 7)
+ylim      = [ (-4, 6), (-4, 6), (-35, 48), ylimt, (-2, 60), ylimt, ylimt ]
+alpha_loc = [  [(20,4.7), (20,3.8)]  , [(20,38), (20,33)]  ,  [(80,4.8), (80,3.7)]   ,    [(80,5.3), (80,4.1)]  ]
+AX        = nlreg1d.plot.plot_multipanel(y, yr, wr, J, colors, ylim, alpha_loc, paired=True, dvlabel='Mediolateral COP  (cm)', group_labels=['Normal', 'Fast'], xlabel=xlabel)
+tx0 = AX[0].text(0.1, 0.8, 'Lateral')
+tx1 = AX[0].text(0.1, 0.1, 'Medial')
+plt.setp( [tx0,tx1], name='Helvetica', size=10, bbox=dict(facecolor='0.9'), transform=AX[0].transAxes)
+[ax.axhline(0, color='k', ls=':')  for ax in AX[:2]]
+plt.show()
+if save:
+	dirFIGS  = os.path.join( pathlib.Path( __file__ ).parent.parent, 'Figures')
+	plt.savefig( os.path.join(dirFIGS, 'datasetD.pdf')  )
